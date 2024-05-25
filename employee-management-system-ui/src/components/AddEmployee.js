b0ea46410ebfafd92135566d9c02616fe8e7cd04
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-05-25 18:21:07 +0530

    "Handling save in React"

--- employee-management-system-ui/src/components/AddEmployee.js
+++ employee-management-system-ui/src/components/AddEmployee.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import EmployeeService from "../services/EmployeeService";
 
 function AddEmployee() {
     const [employee, setEmployee] = useState({
@@ -11,6 +12,15 @@ function AddEmployee() {
     const handleChange = (e) => {
         const value = e.target.value;
         setEmployee({...employee, [e.target.name]: value})
+    };
+
+    const saveEmployee = (e) => {
+        e.preventDefault();
+        EmployeeService.saveEmployee(employee).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        })
     };
 
     return (
@@ -58,6 +68,7 @@ function AddEmployee() {
                 </div>
                 <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
                     <button
+                        onClick={saveEmployee}
                         className="rounded text-white font-semibold bg-green-400
            hover:bg-green-700 py-2 px-2"
                     >
