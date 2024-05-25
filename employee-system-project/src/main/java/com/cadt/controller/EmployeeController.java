76045d6c40c29c11235c883240f5bac942699e01
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-05-25 18:23:34 +0530

    "Added cross origin in employeeController file"

--- employee-system-project/src/main/java/com/cadt/controller/EmployeeController.java
+++ employee-system-project/src/main/java/com/cadt/controller/EmployeeController.java
@@ -3,11 +3,9 @@ package com.cadt.controller;
 import com.cadt.model.Employee;
 import com.cadt.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
