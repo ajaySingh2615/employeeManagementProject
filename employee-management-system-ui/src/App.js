c0e71ef67a19b70256947486f67292ea138c129c
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-05-25 18:51:29 +0530

    "Adding React route 6"

--- employee-management-system-ui/src/App.js
+++ employee-management-system-ui/src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import AddEmployee from "./components/AddEmployee.js";
 import Navbar from "./components/Navbar.js";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import EmployeeList from "./components/EmployeeList";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <AddEmployee />
-    </>
-  );
+    return (<>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route index element={<EmployeeList/>}></Route>
+                <Route path="/" element={<EmployeeList/>}></Route>
+                <Route path="/employeeList" element={<EmployeeList/>}></Route>
+                <Route path="/addEmployee" element={<AddEmployee/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    </>);
 }
 
 export default App;
