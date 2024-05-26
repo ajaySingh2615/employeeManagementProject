4be39885d23b9f42c2ab3b8ad8954f3936902f0e
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-05-26 10:54:29 +0530

    update and cancel component created

--- employee-management-system-ui/src/App.js
+++ employee-management-system-ui/src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
 import AddEmployee from "./components/AddEmployee.js";
 import Navbar from "./components/Navbar.js";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EmployeeList from "./components/EmployeeList";
+import UpdateEmployee from "./components/UpdateEmployee.js";
 
 function App() {
-    return (<>
-        <BrowserRouter>
-            <Navbar/>
-            <Routes>
-                <Route index element={<EmployeeList/>}></Route>
-                <Route path="/" element={<EmployeeList/>}></Route>
-                <Route path="/employeeList" element={<EmployeeList/>}></Route>
-                <Route path="/addEmployee" element={<AddEmployee/>}></Route>
-            </Routes>
-        </BrowserRouter>
-    </>);
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<EmployeeList />}></Route>
+          <Route path="/" element={<EmployeeList />}></Route>
+          <Route path="/employeeList" element={<EmployeeList />}></Route>
+          <Route path="/addEmployee" element={<AddEmployee />}></Route>
+          <Route path="/editEmployee/:id" element={<UpdateEmployee />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
